17ea70caf0e436a09b6347bf7d6fc54b1937c57f
Tashi Jain <tashjain88@gmail.com>
2020-07-13 12:13:59 -0700

    initial commit quiz

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -3,6 +3,7 @@
 //  Quizzler-iOS13
 //
 //  Created by Angela Yu on 12/07/2019.
+//  exercises done by tashi Jain 2020 based on course by Angela
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
@@ -10,11 +11,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    
+    let quiz = [
+            "abc",
+            "bh",
+            "hhu"
+        ]
+    var questionNum = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNum += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNum]
+    }
+    
 }
 
